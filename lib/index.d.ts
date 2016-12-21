1c2bcd67fc5070ccbddc4c5b8e9f3c70ffd8e6e2
Sebastian E. Kwiatkowski <sebastian@gopher.ai>
2016-12-21 01:41:20 +0100

    - Bumped the version number
    - Added missing exports for the new functionality

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,8 @@
 import { Box, box } from './Box';
+import { Lazy, lazy } from './Lazy';
+import { Task, task } from './Task';
+import identity from './Identity';
 export { Box, box };
+export { Lazy, lazy };
+export { Task, task };
+export { identity };
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,9 @@
 import { Box, box } from './Box'
+import { Lazy, lazy } from './Lazy'
+import { Task, task } from './Task'
+import identity from './Identity'
 
 export { Box, box }
+export { Lazy, lazy }
+export { Task, task }
+export { identity }
