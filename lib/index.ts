33b2bb110b4751a7db0a13ab564a97245c12e00b
Sebastian E. Kwiatkowski <sebastian@gopher.ai>
2016-12-27 04:55:07 +0100

    - Bumped the version number
    - Added missing exports for the new Task factory functions

--- lib/index.ts
+++ lib/index.ts
@@ -5,7 +5,7 @@ import { Some, some } from './optional/some'
 import { None, none } from './optional/none'
 import { Lazy, lazy } from './lazy/lazy'
 import { Fork, Resolve, Reject } from './task/fork'
-import { Task, task } from './task/task'
+import { Task, task, rejected, resolved } from './task/task'
 import { Result } from './result/result'
 import { Success, success } from './result/success'
 import { Failure, failure } from './result/failure'
@@ -18,7 +18,7 @@ export { Some, some }
 export { None, none }
 export { Lazy, lazy }
 export { Task, task }
-export { Fork, Resolve, Reject }
+export { Fork, Resolve, Reject, rejected, resolved }
 export { Result }
 export { Success, success }
 export { Failure, failure }
